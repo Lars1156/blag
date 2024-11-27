0cf0d45312c442a8b844ba164dbf6a855c8f6635
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-11-27 16:06:46 +0530

    add the userController File

---
+++ Backend/controller/userController.js
@@ -0,0 +1,50 @@
+const User = require('../model/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = "Kishan@1156";
+
+exports.registerUser = async(req ,res) =>{
+    try {
+        const {userName , email , password , role} = req.body;
+        console.log(req.body);
+        const existingUser = await User.find({email});
+        if(existingUser){
+            return res.status(404).json({msg:"User is Already Exists"});
+        }
+        const newUser = new User({
+            userName,
+            email,
+            password,
+            role
+        });
+        await newUser.save();
+        res.status(201).json({
+            message: 'User registered successfully!',
+            user: {
+              userName: newUser.userName,
+              email: newUser.email,
+              role: newUser.role
+            }
+          });
+    } catch (error) {
+      return   res.status(500).json({ message: 'Error registering user', error: error.message });
+    }
+};
+
+exports.loginUser = async(req,res)=>{
+    try {
+          const{email , password} = req.body;
+          const user = await User.findOne({email});
+          if(!user){
+            return  res.status(400).json({ message: 'Invalid email or password' });
+          }
+          const isMatch = await bcrypt.compare(password, user.password);
+          if(!isMatch){
+            return  res.status(400).json({ message: 'Invalid email or password' });
+          }
+          const token = jwt.sing({userId : user._id , role: user.role}, secret,{expiresIn: '1h'});
+    } catch (error) {
+        return   res.status(500).json({ message: 'Error registering user', error: error.message });
+
+    }
+}
