22ef3b52661cd48cae10784bd483e0f71a139166
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-11-27 15:43:24 +0530

    add the userSchma

---
+++ Backend/model/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// UserSchema
+const userSchema = new mongoose.Schema({
+    userName:{
+        type: String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        match: [/.+\@.+\..+/, 'Please enter a valid email address'],
+    },
+    password:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'author', 'reader'],
+        default: 'reader'
+      },
+      profilePicture: {
+        type: String, 
+      },
+      bio: {
+        type: String,
+        maxLength: 500
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      }
+});
+
+userSchema.pre('save', async function(next){
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+  };
+  
+  const User = mongoose.model('User', userSchema);
+  module.exports = User;
