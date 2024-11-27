4a1eb976a0b923e28a7b8a8e20d706df19c533ae
Kishan Kulkarni <124907215+Lars1156@users.noreply.github.com>
2024-11-27 13:09:39 +0530

    add the chanfes on backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const connection = require('./connection')
 const app = express();
 
 
-
+// Database Connection 
+connection('').then(()=>{
+    console.log("Database Connection Sucessfully");
+    
+}).catch((error)=>{
+    console.log("Database Connection Faild");
+    
+})
 app.listen(8000 , ()=>{
     console.log("Sever is conntected to the 8000 port");
     
